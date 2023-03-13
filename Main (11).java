a3e54f6dbbf4c59a3179f127100cf927a919ed45
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-03-12 20:14:45 -0400

    commit

--- Main (11).java
+++ Main (11).java
@@ -1,65 +1,69 @@
-import java.util.ArrayList; 
-import java.util.Scanner; 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    boolean running = true; 
-   Scanner input = new Scanner(System.in); 
-    ArrayList<Integer> list = new ArrayList<Integer>(); 
-    System.out.println("Type your first number:"); 
-    int a = input.nextInt(); 
-    list.add(a); 
+    boolean running = true;
+    Scanner input = new Scanner(System.in);
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    System.out.println("Type your first number:");
+    int a = input.nextInt();
+    list.add(a);
     System.out.println("Would you like to add to the list? Type 1 for yes and 2 for no.");
-    int runAgain = input.nextInt(); 
-      if (runAgain == 2){
-       running = false; 
-      } else {
-        running = true; 
-      }
+    int runAgain = input.nextInt();
+    if (runAgain == 2) {
+      running = false;
+    } else {
+      running = true;
+    }
     while (running) {
-       System.out.println("Type your next number: "); 
-      a = input.nextInt(); 
-      list.add(a); 
+      System.out.println("Type your next number: ");
+      a = input.nextInt();
+      list.add(a);
       System.out.println("Would you like to add to the list? Type 1 for yes and 2 for no.");
-    runAgain = input.nextInt(); 
-      if (runAgain == 2){
-       running = false; 
+      runAgain = input.nextInt();
+      if (runAgain == 2) {
+        running = false;
         odd(list);
-        even(list); 
+        even(list);
       } else {
-        running = true; 
+        running = true;
       }
     }
-    
+
   }
-  
-  public static void odd(ArrayList<Integer> l){
+
+  public static void odd(ArrayList<Integer> l) {
     ArrayList<Integer> odds = new ArrayList<Integer>();
-    for(int i: l){
-      if (i % 2 == 1){
-        odds.add(i); 
+    for (int i : l) {
+      if (i % 2 == 1) {
+        odds.add(i);
       }
     }
-    int numOdds = odds.size(); 
-    System.out.println("There are " + numOdds + " odd numbers in the array."); 
-    System.out.print("The odd numbers in the array are: "); 
-    for(int x: odds){
-      System.out.print(x + " "); 
+    int numOdds = odds.size();
+    System.out.println("There are " + numOdds + " odd numbers in the array.");
+    System.out.print("The odd numbers in the array are: ");
+    for (int x : odds) {
+      System.out.print(x + " ");
     }
-    System.out.println(); 
+
+    System.out.println();
   }
-  public static void even(ArrayList<Integer> l){
-     ArrayList<Integer> evens = new ArrayList<Integer>();
-     for(int i: l){
-      if (i % 2 == 0){
-        evens.add(i); 
+
+  public static void even(ArrayList<Integer> l) {
+    ArrayList<Integer> evens = new ArrayList<Integer>();
+    for (int i : l) {
+      if (i % 2 == 0) {
+        evens.add(i);
       }
     }
-    int numEvens = evens.size(); 
-    System.out.println("There are " + numEvens + " even numbers in the array."); 
-    System.out.print("The even numbers in the array are: "); 
-    for(int x: evens){
-      System.out.print(x + " "); 
+    int numEvens = evens.size();
+    System.out.println("There are " + numEvens + " even numbers in the array.");
+    System.out.print("The even numbers in the array are: ");
+    for (int x : evens) {
+      System.out.print(x + " ");
     }
-    System.out.println(); 
+
+    System.out.println();
   }
 }
